947fd2e0378fdce4f43f1e96c863cf97cb877908
Reginaldo Barros de Sousa <32406036+revelarock@users.noreply.github.com>
2017-11-18 01:24:26 -0200

    Initial commit

---
+++ app/src/main/java/caracoroa/abctech/com/caraoucoroa/MainActivity.java
@@ -0,0 +1,43 @@
+package caracoroa.abctech.com.caraoucoroa;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView botaoJogar;
+    private String[] opcao = {"cara", "coroa"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botaoJogar = (ImageView) findViewById(R.id.botaoJogarId);
+
+        botaoJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Gerar Número Aleatório
+                Random randomico = new Random();
+                int numeroAleatorio = randomico.nextInt(2);
+
+                // 0 - Cara - 1 coroa
+
+                Intent intent = new Intent(MainActivity.this, DetalheActivity.class);
+                intent.putExtra("opcao", opcao[numeroAleatorio]);
+
+                startActivity(intent);
+
+
+
+            }
+        });
+    }
+}
